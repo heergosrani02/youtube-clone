1aef5b5df106a5de21f60f69262933162269108c
Heer Gosrani <gosraniheer@gmail.com>
2025-06-02 17:41:40 +0530

    sonething-fixed

--- src/components/homeSection/homeSection.jsx
+++ src/components/homeSection/homeSection.jsx
@@ -1,10 +1,23 @@
 import homeCss from "./homeSection.module.css";
 import { useEffect, useState } from "react";
 import { API_KEY } from "./../../data.js";
+import moment from "moment";
+
+const value_converter  = (value) => {
+  if(value>=1000000){
+    return Math.floor(value/1000000)+"M";
+  }
+  else if(value>=1000){
+    return Math.floor(value/1000)+"K";
+  }
+  else{
+    return value;
+  }
+}
 
 function HomeSection() {
   const [videos, setVideos] = useState([]);
-  const videoUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&regionCode=IN&key=${API_KEY}`;
+  const videoUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=30&regionCode=US&key=${API_KEY}`;
 
   useEffect(() => {
     fetch(videoUrl)
@@ -22,7 +35,9 @@ function HomeSection() {
               <div className={homeCss.items}> 
                 <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title}/>
                 <p>{video.snippet.title}</p>
-                <span>{video.snippet.channelTitle}</span>
+                <span>{video.snippet.channelTitle}</span> <br />
+                <span>{value_converter(video.statistics.viewCount)} views &bull; </span>
+                <span>{moment(video.snippet.publishedAt).fromNow()}</span>
               </div>
             </div>
           );
--- src/data.js
+++ src/data.js
@@ -125,4 +125,4 @@ export const filter = [
   },
 ];
 
-export const API_KEY = "AIzaSyACpsWP3k4zgH7StVkV6pPODtEP_KoRlNE";
+export const API_KEY = 'AIzaSyACpsWP3k4zgH7StVkV6pPODtEP_KoRlNE';
