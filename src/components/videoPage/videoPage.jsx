700c4617cc51f61fec9401796c1a223ea3cb5f24
Heer Gosrani <gosraniheer@gmail.com>
2025-06-17 19:31:02 +0530

    done

--- src/components/videoPage/videoPage.jsx
+++ src/components/videoPage/videoPage.jsx
@@ -44,7 +44,7 @@ function VideoPage(){
 
     useEffect(() => {
         fetchVideoDetail();
-    }, [channelData, commentsData])
+    }, [videoData, channelData, commentsData]);
 
     return(
         <>
